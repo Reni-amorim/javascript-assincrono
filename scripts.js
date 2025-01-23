14d30457d7d68700a279f03b6ceb4f22d527e8fe
Reni-amorim <reni.425@gmail.com>
2025-01-22 23:42:13 -0300

    function to add and remove tags, partial tags search functions

--- scripts.js
+++ scripts.js
@@ -36,3 +36,41 @@ inputUpload.addEventListener("change", async (evento) => {
         }
     }
 })
+
+const inputTags = document.getElementById("input-tags");
+const listaTags = document.getElementById("lista-tags");
+
+inputTags.addEventListener("keypress", (evento) => {
+    if (evento.key === "Enter") {
+        evento.preventDefault();
+        const tagTexto = inputTags.value.trim();
+        if (tagTexto !== "") {
+            const tagNova = document.createElement("li");
+            tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
+            listaTags.appendChild(tagNova);
+            inputTags.value = "";
+        }
+    }
+})
+
+ listaTags.addEventListener("click", (evento) => {
+    // Verica o local clicado
+    if (evento.target.classList.contains("remove-tag")) {
+        // pega o elemento pai do elemento clicado. No caso o li
+        const tagToRemove = evento.target.parentElement;
+        // Pega o elemento filho e remove através do removechild
+        listaTags.removeChild(tagToRemove);
+    }
+ })
+
+ const tagsDisponiveis = ["Front-end", "Back-end", "Fullstack", "Mobile", "DevOps"]
+
+//Async function para buscar as tags e retornar um promise
+ async function buscarTags(tagtexto) {
+    return new Promise((resolve) => {
+        //set time out para simular uma busca
+        setTimeout(() => {
+            resolve(tagsDisponiveis.includes(tagtexto));
+        }, 1000)        
+    })
+} 
